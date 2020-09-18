a129ef71387cbeac0cc4e6fdd43d558a822bd783
Guidoge3 <70884054+Guidoge3@users.noreply.github.com>
2020-09-18 11:03:34 +0200

    Add files via upload

---
+++ 01-Turtle/Turtle.py
@@ -0,0 +1,13 @@
+import turtle
+
+for x in range(10):
+    turtle.forward(120)
+    turtle.right(290)
+    turtle.forward(120)
+    turtle.right(290)
+    turtle.forward(120)
+    turtle.right(290)
+    turtle.forward(120)
+    turtle.right(290)
+    
+turtle.done()
